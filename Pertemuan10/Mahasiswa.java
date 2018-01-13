f5762bbb906866d0f7901f1c777721242601889b
Chepy Abdul Ghopur <chepy.abdulg@gmail.com>
2018-01-13 19:52:08 +0700

    Add files via upload

---
+++ Pertemuan10/Mahasiswa.java
@@ -0,0 +1,12 @@
+public abstract class Mahasiswa{
+    protected String nama ="Cepi";
+    protected String nim ="15111193";
+    
+    protected String getNama() {
+    return nama;
+    }
+    protected String getNim() {
+    return nim;
+    }
+    public abstract void printNama();
+}
